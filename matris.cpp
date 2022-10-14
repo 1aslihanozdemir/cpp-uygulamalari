14f1014c8b969af7a424e702fcfd094bcd87a46f
Aslıhan Özdemir <aslihan.ozdemir2@ogr.sakarya.edu.tr>
2022-10-14 23:12:11 +0300

    Create matris.cpp

---
+++ matris.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int matris [3][3];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cin >> matris[i][j];
+			
+		}
+	}
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(i==1 && j==0) cout << endl;
+			else if(i==2 && j==0) cout << endl;
+			cout << matris[i][j];
+			
+
+			
+		}
+	}
+	return 0;
+
+}
